6e4ef0df61c2fc3b23797f4175a7a773f8be23e7
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-08 15:15:53 +0330

    update Post model

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -8,12 +9,19 @@ class Category(models.Model):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ("d", "draft"),
+        ("p", "published"),
+    )
     author = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, related_name="author-post"
+        get_user_model(), on_delete=models.CASCADE, related_name="author"
     )
     title = models.CharField(max_length=100)
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, related_name="category-post"
+        Category, on_delete=models.SET_NULL, null=True, related_name="category"
     )
     body = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="d")
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(default=timezone.now)
