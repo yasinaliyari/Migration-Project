db68bb00ceaf23ecbdaaa69dbf9c92540a4278fb
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-08 14:56:03 +0330

    refactor author field

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Post(models.Model):
-    author = models.CharField(max_length=50)
+    # author = models.CharField(max_length=50)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="author-post"
+    )
     title = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     body = models.TextField()
