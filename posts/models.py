8b8183d82422914ef4ea26ff1d931f0aa26fb8f8
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-08 15:03:27 +0330

    refactor category field

--- posts/models.py
+++ posts/models.py
@@ -2,17 +2,18 @@ from django.contrib.auth import get_user_model
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    status = models.BooleanField(default=True)
+
+
 class Post(models.Model):
-    # author = models.CharField(max_length=50)
     author = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE, related_name="author-post"
     )
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=50)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, related_name="category-post"
+    )
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-
-
-class Category(models.Model):
-    title = models.CharField(max_length=50)
-    status = models.BooleanField(default=False)
