cdd049365c0dfb48d93d58184e7aa0c3ec803dbe
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-08 14:51:41 +0330

    create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
